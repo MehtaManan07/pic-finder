aa8115555742ca421479d1982f094ac1b0e0bfb8
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-05-25 16:34:44 +0530

    Updates

---
+++ precache-manifest.95d0c71e6b8a4e42c6a25c7d7f75096c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f152d53f49f9a9bb1c422fff4e98a66c",
+    "url": "/pic-finder/index.html"
+  },
+  {
+    "revision": "aa5fd15549cccc87b82e",
+    "url": "/pic-finder/static/css/main.dfef24dd.chunk.css"
+  },
+  {
+    "revision": "d5515288e2e7ff66c9e1",
+    "url": "/pic-finder/static/js/2.5e4a987b.chunk.js"
+  },
+  {
+    "revision": "971b4e562d3abe5fe0995509ffca7edb",
+    "url": "/pic-finder/static/js/2.5e4a987b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa5fd15549cccc87b82e",
+    "url": "/pic-finder/static/js/main.41ee7229.chunk.js"
+  },
+  {
+    "revision": "519c8e248280ddad2ad6",
+    "url": "/pic-finder/static/js/runtime-main.4ebc1f84.js"
+  }
+]);
